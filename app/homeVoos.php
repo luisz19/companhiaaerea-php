67df7dd84060e2e3246b6cb27fe0f9a288f77300
João Filho <joaodaviabc@gmail.com>
2024-05-22 23:34:38 -0300

    login registro homevoos

--- app/homeVoos.php
+++ app/homeVoos.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 
 // // Verifica se o usuário não está autenticado, se não estiver, redireciona para a página de login
@@ -6,12 +6,12 @@ session_start();
 //     header('Location: trooll.php');
 //     exit();
 // }
-
-
+require_once './database/conexao.php';
 
 ?>
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -34,7 +34,109 @@ session_start();
         }
     </script> -->
 </head>
+
 <body>
-    <h1>FOII</h1>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    </head>
+
+    <body class="bg-dark">
+        <div class="main-page w-100 h-100">
+            <div class="main-page-container d-flex bg-white mx-auto my-4 flex-column px-5 py-5" style="max-width: 1080px">
+                <div class="main-page-container-title pb-5">
+                    <h1 class="text-black">Escolha o seu voo:</h1>
+                </div>
+                <div class="voo-section d-flex gap-3">
+                    <div class="voo-section-container d-flex justify-content-center">
+                        <div class="voo-section-title d-flex flex-column gap-3 bg-dark px-4 py-3 w-auto text-white align-items-center">
+                            <div class="d-flex align-items-center gap-3">
+                                <p class="de-onde-text fs-4">
+                                    <php? $voo>
+                                </p>
+                                <p>para</p>
+                                <p class=" para-onde-text fs-4">São Paulo</p>
+                            </div>
+                            <div class="d-flex gap-3">
+                                <p>13:30</p>
+                                <p>12:00</p>
+                            </div>
+                            <div class="botao">
+                                <button>Escolher voo</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="voo-section-container d-flex justify-content-center">
+                        <div class="voo-section-title d-flex flex-column gap-3 bg-dark px-4 py-3 w-auto text-white align-items-center">
+                            <div class="d-flex align-items-center gap-3">
+                                <p class="de-onde-text fs-4">Juazeiro</p>
+                                <p>para</p>
+                                <p class=" para-onde-text fs-4">São Paulo</p>
+                            </div>
+                            <div class="d-flex gap-3">
+                                <p>13:30</p>
+                                <p>12:00</p>
+                            </div>
+                            <div class="botao">
+                                <button>Escolher voo</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="voo-section-container d-flex justify-content-center">
+                        <div class="voo-section-title d-flex flex-column gap-3 bg-dark px-4 py-3 w-auto text-white align-items-center">
+                            <div class="d-flex align-items-center gap-3">
+                                <p class="de-onde-text fs-4">Juazeiro</p>
+                                <p>para</p>
+                                <p class=" para-onde-text fs-4">São Paulo</p>
+                            </div>
+                            <div class="d-flex gap-3">
+                                <p>13:30</p>
+                                <p>12:00</p>
+                            </div>
+                            <div class="botao">
+                                <button>Escolher voo</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+
+    </html>
 </body>
+
 </html>
+<!-- 
+<section>
+    <div class="row">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Número do Voo</th>
+                    <th>Origem</th>
+                    <th>Destino</th>
+                </tr>
+            </thead>
+            <tbody>
+                foreach ($dados as $dado) : ?>
+                    <tr>
+                        <td> echo $dado['num_voo']; ?></td>
+                        <td> echo $dado['origem']; ?></td>
+                        <td> echo $dado['destino']; ?></td>
+                    </tr>
+                 endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</section> -->
--- app/registro.php
+++ app/registro.php
@@ -1,25 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../app/css/login.css">
     <title>Entrar na Conta</title>
-</head>
-<body>
-<form action="registro_user.php" method="post">
-    <div class="login">
-        <h1>Criar Conta</h1>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-        <input type="text" placeholder="Nome" name="nome">
-        <input type="email" placeholder="Email" name="email">
-        <input type="number" placeholder="CPF" name="cpf">
-        <input type="tel" placeholder="Telefone" name="tel">
-        <input type="number" placeholder="Número do passaporte" name="num_passaporte">
-        <a href="login.php">Já tem uma conta faça login!</a>
-        <br>
-        <button class="login" type="submit">Criar</button>
+</head>
+
+<body class="bg-dark">
+    <div class="registro-container px-5 py-5 mx-auto mt-5 " style="max-width: 600px;">
+        <h1 class="text-light pb-5">Crie sua conta</h1>
+        <form action="registro_user.php" method="post" class="text-light">
+
+
+            <!-- Name input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="text" id="registerName" class="form-control" />
+                <label class="form-label" for="registerName">Name</label>
+            </div>
+
+            <!-- Username input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="text" id="registerUsername" class="form-control" />
+                <label class="form-label" for="registerUsername">Username</label>
+            </div>
+
+            <!-- Email input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="email" id="registerEmail" class="form-control" />
+                <label class="form-label" for="registerEmail">Email</label>
+            </div>
+
+            <!-- Password input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="password" id="registerPassword" class="form-control" />
+                <label class="form-label" for="registerPassword">Password</label>
+            </div>
+
+            <!-- Repeat Password input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="password" id="registerRepeatPassword" class="form-control" />
+                <label class="form-label" for="registerRepeatPassword">Repeat password</label>
+            </div>
+
+            <!-- Checkbox -->
+
+
+
+            <!-- Submit button -->
+            <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-3">Registrar</button>
+            <br>
+            <a href="login.php">Já tem uma conta faça login!</a>
+        </form>
     </div>
-</form>
 </body>
+
 </html>
